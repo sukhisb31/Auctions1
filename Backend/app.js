01b7233c9cbbac7af8bba2d40090e4115a8f628d
[Sukhwinder Singh] <[sukhisb31@gmail.coml]>
2025-03-22 22:31:45 +0530

    till commission routes

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,9 @@ import { connection } from "./database/connection.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import userRouter from "./routers/userRouter.js";
 import auctionItemRouter from "./routers/auctionItemRoutes.js";
-import bidRouter from "./routers/bidRoutes.js"
+import bidRouter from "./routers/bidRoutes.js";
+import commissionRouter from "./routers/commissionRouter.js";
+
 const app = express();
 config({
     path: "./config/.env"
@@ -30,6 +32,8 @@ app.use(fileUpload({
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auctionitem", auctionItemRouter);
 app.use("/api/v1/bid", bidRouter);
+app.use("/api/v1/commission", commissionRouter);
+
 
 connection();
 app.use(errorMiddleware);
