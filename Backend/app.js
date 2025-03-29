fc53e44272a421db0392705d99c7c30bf5e7090d
[Sukhwinder Singh] <[sukhisb31@gmail.coml]>
2025-03-29 23:55:30 +0530

    till admin user

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,7 @@ import userRouter from "./routers/userRouter.js";
 import auctionItemRouter from "./routers/auctionItemRoutes.js";
 import bidRouter from "./routers/bidRoutes.js";
 import commissionRouter from "./routers/commissionRouter.js";
+import superAdminRouter from "./routers/superAdminRoutes.js"; 
 
 const app = express();
 config({
@@ -33,6 +34,7 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auctionitem", auctionItemRouter);
 app.use("/api/v1/bid", bidRouter);
 app.use("/api/v1/commission", commissionRouter);
+app.use("/api/v1/superadmin", superAdminRouter);
 
 
 connection();
